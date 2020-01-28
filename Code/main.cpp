1a445349db9f8a51836fda50b1ebbad7703893a0
RaspberryPi <2198699s@student.gla.ac.uk>
2020-01-28 15:54:32 +0000

    first commit from raspberry pi

--- Code/main.cpp
+++ Code/main.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 int main() 
 {
-    cout << "Welcome to Pitch Perfector!";
+    cout << "Welcome to Pitch Perfector!\n";
+    cout << "This code has been written on the Raspberry Pi\n";
     return 0;
 }
