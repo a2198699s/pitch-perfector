9a7ccfc8a3862f31afd8471f2e8708b822da91fd
HanozBhamgara <2191455b@student.gla.ac.uk>
2020-01-28 16:23:41 +0000

    Updated main.cpp with some audio code

--- Code/main.cpp
+++ Code/main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+#include <SFML/Audio.hpp>
+
 using namespace std;
 int main() 
 {
     cout << "Welcome to Pitch Perfector!\n";
     cout << "This code has been over-written on the Raspberry Pi\n";
+    
+    sf::Music music;
+    if (!music.openFromFile("music.wav")) 
+        return -1; // error
+    music.play();
     return 0;
 }
