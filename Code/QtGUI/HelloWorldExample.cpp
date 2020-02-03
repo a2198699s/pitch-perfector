c25a2ad1758c8374d36823077410acf3d6c7bf6f
RaspberryPi <pitch-perfect-raspi@gmail.com>
2020-02-03 14:39:00 +0000

    added basic qt gui program and makefiles

---
+++ Code/QtGUI/HelloWorldExample.cpp
@@ -0,0 +1,10 @@
+#include <QtGui>
+#include <QApplication>
+#include <QLabel>
+
+int main (int argc, char **argv) {
+    QApplication app (argc, argv);
+    QLabel label("hello world");
+    label.show();
+    return app.exec();
+}
