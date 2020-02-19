9192d7146f0a5e5c6c1b4fe0f659e34f030cd4b8
Noah Telerman <60071670+N0AHt@users.noreply.github.com>
2020-02-19 11:37:03 +0000

    added sawtooth to duplex test code

--- Code/duplex_rtaudio_testing.cpp
+++ Code/duplex_rtaudio_testing.cpp
@@ -2,6 +2,27 @@
 #include <iostream>
 #include <cstdlib>
 #include <cstring>
+
+// Two-channel sawtooth wave generator.
+int saw( void *outputBuffer, void *inputBuffer, unsigned int nBufferFrames,
+         double streamTime, RtAudioStreamStatus status, void *userData )
+{
+  unsigned int i, j;
+  double *buffer = (double *) outputBuffer;
+  double *lastValues = (double *) userData;
+  if ( status )
+    std::cout << "Stream underflow detected!" << std::endl;
+  // Write interleaved audio data.
+  for ( i=0; i<nBufferFrames; i++ ) {
+    for ( j=0; j<2; j++ ) {
+      *buffer++ = lastValues[j];
+      lastValues[j] += 0.005 * (j+1+(j*0.1));
+      if ( lastValues[j] >= 1.0 ) lastValues[j] -= 2.0;
+    }
+  }
+  return 0;
+}
+
 // Pass-through function.
 int inout( void *outputBuffer, void *inputBuffer, unsigned int nBufferFrames,
            double streamTime, RtAudioStreamStatus status, void *data )
