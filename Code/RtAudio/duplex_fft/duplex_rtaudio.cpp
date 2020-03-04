3a86b535ac54b2d8e3e21197f1440d59bb7b2e85
RaspberryPi <pitch-perfect-raspi@gmail.com>
2020-03-04 10:53:44 +0000

    Working fft on audio saving to file

--- Code/RtAudio/duplex_fft/duplex_rtaudio.cpp
+++ Code/RtAudio/duplex_fft/duplex_rtaudio.cpp
@@ -12,51 +12,23 @@ ofstream outputFile("audio_out.txt");
 ofstream inputFile("input_data.txt");
 
 
-class fft {        
-  public:
-    int nBufferFrames;
-    double *in;
-    fftw_complex *out;
-    int flag;
-    fftw_plan my_plan;
-
-    fft(int nBufferFrames) {
-	in = (double *) fftw_malloc(sizeof(double)*nBufferFrames);
-	out = (fftw_complex *) fftw_malloc(sizeof(fftw_complex)*nBufferFrames);
-	my_plan = fftw_plan_dft_r2c_1d(nBufferFrames, in, out, FFTW_MEASURE);
-    };
-
-
-    void executefft(void *outputBuffer, void *inputBuffer, unsigned int nBufferFrames, double streamTime, RtAudioStreamStatus status, void *data) {
-	memcpy(in, (double *)inputBuffer, *(unsigned long*)data);
-	fftw_execute(my_plan);
-    };
-  
-};
-
 int inout( void *outputBuffer, void *inputBuffer, unsigned int nBufferFrames,
            double streamTime, RtAudioStreamStatus status, void *data )
 {
   fftw_complex *out;
   double *in;
   fftw_plan my_plan;
-  // Since the number of input and output channels is equal, we can do
-  // a simple buffer copy operation here.
   if ( status ) std::cout << "Stream over/underflow detected." << std::endl;
   unsigned long *bytes = (unsigned long *) data;
   double *audio_in  = (double *) inputBuffer;
-  // float *audio_out = (float *) outputBuffer; 
   int signedNBufferFrames = (int) nBufferFrames;
-
 
   in = (double *) fftw_malloc(sizeof(double)*signedNBufferFrames);
   out = (fftw_complex *) fftw_malloc(sizeof(fftw_complex)*signedNBufferFrames);
   my_plan = fftw_plan_dft_r2c_1d(signedNBufferFrames, in, out, FFTW_ESTIMATE  );
 
-
   memcpy(in, audio_in, *bytes);
   fftw_execute(my_plan);
-
 
   for (int x=0; x < nBufferFrames; x++) {
       inputFile << audio_in[x] << "\n";
@@ -65,20 +37,8 @@ int inout( void *outputBuffer, void *inputBuffer, unsigned int nBufferFrames,
         outputFile << *out[x] << " ";
   }
   outputFile << "\n" << endl;
-  // fftw_free(in)
-  // fftw_destroy_plan(my_plan);
 
-
-
-  // for (int x=0; x < nBufferFrames; x++) {
-  //   outputFile << in[x];
-  //   outData << out[x];
-
-  // }
-  // outputFile << endl;
-  // outData << endl;
-
-  // //destroy plan and buffers?
+  /// Destroys fft plan each iteration. Needs to be removed.
   fftw_destroy_plan(my_plan);
   fftw_free(in); fftw_free(out);
 
@@ -99,6 +59,7 @@ int main()
   iParams.nChannels = 1;
   oParams.deviceId = 0; // first available device
   oParams.nChannels = 1;
+
   try {
     adac.openStream( &oParams, &iParams, RTAUDIO_FLOAT64, 44100, &bufferFrames, &inout, (void *)&bufferBytes );
   }
