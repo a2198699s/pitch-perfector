a1996122af1378e595b712e3dace97e12e635d6b
RaspberryPi <pitch-perfect-raspi@gmail.com>
2020-03-11 10:06:25 +0000

    fft class fix

--- Code/RtAudio/fft_object/fft.cpp
+++ Code/RtAudio/fft_object/fft.cpp
@@ -12,12 +12,11 @@ class fft {
       in = (double *) fftw_malloc(sizeof(double)*nBufferFrames);
       out = (fftw_complex *) fftw_malloc(sizeof(fftw_complex)*nBufferFrames);
       my_plan = fftw_plan_dft_r2c_1d(nBufferFrames, in, out, FFTW_MEASURE);
+      this->nBufferFrames = nBufferFrames;
     };
 
-
     void executefft(double *inputBuffer) {
-      memcpy(in, inputBuffer, sizeof(in));
+      memcpy(in, inputBuffer, sizeof(double)*nBufferFrames );
       fftw_execute(my_plan);
     };
 };
-
