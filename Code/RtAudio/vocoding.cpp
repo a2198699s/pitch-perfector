120690dd33bfa691959deea3f8931c0fb9cdd9a4
Noah Telerman <60071670+N0AHt@users.noreply.github.com>
2020-03-11 11:53:14 +0000

    began vocoder

---
+++ Code/RtAudio/vocoding.cpp
@@ -0,0 +1,141 @@
+#include <RtAudio.h>
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <cstring>
+#include <fftw3.h>
+// #include "fft.h"
+using namespace std;
+
+ofstream outputFile("audio_out.txt");
+ofstream inputFile("input_data.txt");
+
+class fft {        
+  public:
+    int nBufferFrames;
+    double *in;
+    fftw_complex *out;
+    int flag;
+    fftw_plan my_plan;
+
+    fft(int nBufferFrames) {
+      in = (double *) fftw_malloc(sizeof(double)*nBufferFrames);
+      out = (fftw_complex *) fftw_malloc(sizeof(fftw_complex)*nBufferFrames);
+      my_plan = fftw_plan_dft_r2c_1d(nBufferFrames, in, out, FFTW_MEASURE);
+      this->nBufferFrames = nBufferFrames;
+    };
+
+    void executefft(double *inputBuffer) {
+      //find datasize in the constructor to reduce runtime calculations in stream
+      memcpy(in, inputBuffer, sizeof(double)*nBufferFrames );
+      fftw_execute(my_plan);
+    };
+};
+
+
+class vocoder {
+  public:
+    float baseFreq;
+    int samplerate;
+    int[] scaleFreqs;
+    float newFreq;
+    fftw_complex* FourierTransform;
+
+    //what should be constructed?
+    vocoder(int samplerate_input, int[] scaleFreqs_input) {
+      this->samplerate = samplerate_input;
+      //hertz per sample?
+      this->scaleFreqs = scaleFreqs_input;
+      
+    };
+
+    void pitchShift_setup(fftw_complex* fft_spectrum) {
+      this->FourierTransform = fft_spectrum;
+
+      //peaks are defined as larger than the 2 bins on either side
+      peaks
+      baseFreq = 
+
+
+      //find nearest note (newFreq)
+
+    };
+
+    void pitchShift{
+      //perform pitch shift??
+    };
+   
+};
+
+
+class dispatch  {
+  public:
+    static int caller(void *outputBuffer, void *inputBuffer, unsigned int nBufferFrames, double streamTime, RtAudioStreamStatus status, void *data) { 
+      fft *fourier = (fft *) data;
+      double *input = (double *) inputBuffer;
+      fourier->executefft(input);
+
+      //write fft to file
+      for (int x=0; x < nBufferFrames; x++) {
+        inputFile << *(fourier->out[x]) << ", ";
+      }
+      inputFile << "\n";
+
+      //return audio to output buffer for playback
+      memcpy(outputBuffer, inputBuffer, sizeof(double)*nBufferFrames);
+      return 0;
+    };
+};
+
+
+int main() {
+
+ RtAudio adac;
+  if ( adac.getDeviceCount() < 1 ) {
+    std::cout << "\nNo audio devices found!\n";
+    exit( 0 );
+  }
+  // Set the same number of channels for both input and output.
+  unsigned int bufferBytes, bufferFrames = 512; // samples/Fs = bufferTime 
+  RtAudio::StreamParameters iParams, oParams;
+  iParams.deviceId = 3; // first available device
+  iParams.nChannels = 1;
+  oParams.deviceId = 0; // first available device
+  oParams.nChannels = 1;
+
+  // Instantiate FFT Class (and others)
+  //do we need a dispatch object? 
+  dispatch dispatcher;
+  int signed_bufferFrames = (int) bufferFrames;
+  fft fourier = fft(signed_bufferFrames);
+  
+
+  try {
+    adac.openStream( &oParams, &iParams, RTAUDIO_FLOAT64, 44100, &bufferFrames, &dispatch::caller , (void *)&fourier );
+  }
+  catch ( RtAudioError& e ) {
+    e.printMessage();
+    exit( 0 );
+  }
+  bufferBytes = bufferFrames * 2 * 4;
+  try {
+    adac.startStream();
+    char input;
+    std::cout << "\nRunning ... press <enter> to quit.\n";
+    std::cin.get(input);
+    // Stop the stream.
+    adac.stopStream();
+  }
+  catch ( RtAudioError& e ) {
+    e.printMessage();
+    goto cleanup;
+  }
+ cleanup:
+  if ( adac.isStreamOpen() ) adac.closeStream();
+  outputFile.close();
+  inputFile.close();
+
+  //Delete our own objects and clean up here!!
+
+  return 0;
+}
