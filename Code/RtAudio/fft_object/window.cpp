976b7e68d9c1231ec544b0b1120cecf45ae558c0
HanozBhamgara <2191455b@student.gla.ac.uk>
2020-03-17 00:05:15 +0000

    removing extras from GUI + squaring frequency values and removing negatives

--- Code/RtAudio/fft_object/window.cpp
+++ Code/RtAudio/fft_object/window.cpp
@@ -6,13 +6,13 @@
 
 Window::Window() : gain(5), count(0)
 {
-	knob = new QwtKnob;
+	// knob = new QwtKnob;
 	// set up the gain knob
-	knob->setValue(gain);
+	// knob->setValue(gain);
 
 	// use the Qt signals/slots framework to update the gain -
 	// every time the knob is moved, the setGain function will be called
-	connect( knob, SIGNAL(valueChanged(double)), SLOT(setGain(double)) );
+	// connect( knob, SIGNAL(valueChanged(double)), SLOT(setGain(double)) );
 
 	// set up the thermometer
 	thermo = new QwtThermo; 
@@ -49,8 +49,8 @@ Window::Window() : gain(5), count(0)
 
 	// set up the layout - knob above thermometer
 	vLayout = new QVBoxLayout;
-	vLayout->addWidget(knob);
-	vLayout->addWidget(thermo);
+	// vLayout->addWidget(knob);
+	// vLayout->addWidget(thermo);
 
 	// plot to the left of knob and thermometer
 	hLayout = new QHBoxLayout;
@@ -87,7 +87,11 @@ void Window::timerEvent( QTimerEvent * )
 
 	// Fill ydata2 up with the real part of fftw_complex.
 	for ( int i=0; i<plotDataSize; ++i ) {
-		yData2[i] = aStreamer->outputData[i][0];
+		if (aStreamer->outputData[i][0] < 0) {
+			yData2[i] = 0;
+			continue;
+		}
+		yData2[i] = aStreamer->outputData[i][0] * aStreamer->outputData[i][0];
 	}
 	curve2->setSamples(xData2, yData2, plotDataSize);
 	plot2->replot();
