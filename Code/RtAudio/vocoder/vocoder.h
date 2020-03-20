0ce206bfd65a2d998bd22282486141d8ab5ba99e
Noah Telerman <n.telerman@gmail.com>
2020-03-20 14:30:57 +0000

    filled class and header, edited down code

---
+++ Code/RtAudio/vocoder/vocoder.h
@@ -0,0 +1,24 @@
+#include <cstdlib>
+#include <fftw3.h>
+
+class vocoder {
+  public:
+    float baseFreq;
+    int baseSample;
+    int samplerate;
+    int bufferSize;
+    float FreqRes;
+    float* scaleFreqs;
+    float newFreq;
+    string Note;
+    fftw_complex* FourierTransform;
+
+    vocoder(int samplerate_input, int bufferSize_input, float* scaleFreqs_input);
+    static int binary_search(const float* NotesInKey, float* note, int highest_index, int lowest_index);
+    static int noteFinder(const float* NotesInKey, float* note);
+    float SampleToFreq(int sample);
+    int NearestNote(float* freq);
+    void pitchShift_setup(fftw_complex* fft_spectrum);
+    void pitchShift(int binDifference);
+
+};
