c506b9253e0957e935e523d44e8d859d1e077ae7
Noah Telerman <n.telerman@gmail.com>
2020-04-15 13:10:53 +0100

    dispatch class and header (untested)

--- Code/classfiles/dispatch.cpp
+++ Code/classfiles/dispatch.cpp
@@ -1,10 +1,21 @@
 #include "fft.h"
 #include <RtAudio.h>
 
+
+Dispatch::Dispatch(fft* fourierPtr, vocoder* vocoderPtr){
+    this->fourierObj = fourierPtr;
+    this->vocoderObj = vocoderPtr;
+};
+
 int Dispatch::caller(void *outputBuffer, void *inputBuffer, unsigned int nBufferFrames, double streamTime, RtAudioStreamStatus status, void *data) {
-    fft *fourier = (fft *) data;
-    double *input = (double *) inputBuffer;
-    fourier->executefft(input);
+    Dispatch* dispatcher = (Dispatch*) data;
+
+    (dispatcher->fourierObj)->executefft((double*) inputBuffer);
+    (dispatcher->vocoderObj)->pitchShift_setup((dispatcher->fourierObj)->out);
+    (dispatcher->vocoderObj)->pitchShift();
+    (dispatcher->fourierObj)->executeInverse_fft((dispatcher->vocoderObj)->FourierTransform);
+
+    inputBuffer = (void*) dispatcher->fourierObj->inverse_out;
     memcpy(outputBuffer, inputBuffer, sizeof(double)*nBufferFrames);
     return 0;
 };
--- Code/classfiles/dispatch.h
+++ Code/classfiles/dispatch.h
@@ -1,7 +1,12 @@
 #include "fft.h"
+#include "vocoder.h"
 #include <RtAudio.h>
 
 class Dispatch  {
   public:
+    fft* fourierObj
+    vocoder* vocoderObj
+
+    Dispatch(fft* fourierPtr, vocoder* vocoderPtr);
     static int caller(void *outputBuffer, void *inputBuffer, unsigned int nBufferFrames, double streamTime, RtAudioStreamStatus status, void *data);
 };
