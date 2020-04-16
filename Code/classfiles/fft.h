2e69c51de36e320b5cbaf558e8847f0dc4aca754
Noah Telerman <n.telerman@gmail.com>
2020-04-16 20:42:57 +0100

    Doxygen comments for the fft header

--- Code/classfiles/fft.h
+++ Code/classfiles/fft.h
@@ -1,3 +1,14 @@
+/** \class fft
+* \brief Class for performing fourier transforms usinf FFTW3
+*
+* Allows for the set-up of a fftw3 plan (the most time consuming
+* part of the process) when the class instantiated, allowing the
+* performance of forward and inverse fast fourier transforms with
+* minimal computational cost inside the RtAudio Callback when the
+* method are called.
+*/
+
+
 #include <RtAudio.h>
 #include <fftw3.h>
 
@@ -13,7 +24,17 @@ class fft {
     fftw_plan my_plan;
     fftw_plan inverse_plan;
 
+    /// Construct the fftw3 plans for the object
     fft(int nBufferFrames);
+
+    /** Execute the forward fourier transform and update
+    * generate the frequncy spectrum for the data input
+    */
     void executefft(double* inputBuffer);
+
+    /** Execute the inverse fourier transform to reconstruct
+    * a time-series signal from the input frequency spectrum.
+    * Data is then scaled back to the appropriate size.
+    */
     void executeInverse_fft(fftw_complex* fourierSpectrum);
 };
