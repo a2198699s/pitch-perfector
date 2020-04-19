344cefd5943eedf36bb2d80c21c68997872b1626
Travis CI <travis@travis-ci.org>
2020-04-19 21:58:28 +0000

    Deploy code docs to GitHub Pages Travis build: 128
    Commit: 78e837f34fe12406d82a696c53c7405e21a72c4d

--- html/search/functions_3.js
+++ html/search/functions_3.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['fft',['fft',['../classfft.html#a5fff94ac07119207ddd05f628be5891d',1,'fft']]]
+  ['fft',['fft',['../classfft.html#a5fff94ac07119207ddd05f628be5891d',1,'fft::fft(int nBufferFrames)'],['../classfft.html#a57f8fc9224851c0a827b3d46102f1623',1,'fft::fft(int nBufferFrames, int samplingRate)']]]
 ];
